31962e4a44da09883de40058a7089312ece440ca
Magd-Elmohandes <ElmohandesMagd@gmail.com>
2023-03-21 16:19:05 -0700

    ROW

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute of a number
+ * @n: the int to convert
+ * Return: the absolute of any number
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
